625b770ca8dd929b971d947ba75ebd8b025e337e
Vitor Bertti <berttivitor@gmail.com>
2020-09-15 22:44:19 -0300

    Added desktop-header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { LeftColumnModule } from './left-column/left-column.module';
 import { MiddleColumnModule } from './middle-column/middle-column.module';
 import { RightColumnModule } from './right-column/right-column.module';
 import { MobileHeaderComponent } from './mobile-header/mobile-header.component';
+import { DesktopHeaderComponent } from './desktop-header/desktop-header.component';
 
 @NgModule({
-  declarations: [AppComponent, LayoutComponent, MobileHeaderComponent],
+  declarations: [AppComponent, LayoutComponent, MobileHeaderComponent, DesktopHeaderComponent],
   imports: [
     BrowserModule,
     LeftColumnModule,
